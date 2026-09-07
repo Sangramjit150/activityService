9bdf4bc6f739598006e8695f17d86381be333c0f
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-09-07 20:28:38 +0530

    This is the request dto

--- src/main/java/com/fitness/activityService/dto/ActivityRequest.java
+++ src/main/java/com/fitness/activityService/dto/ActivityRequest.java
@@ -1,4 +1,15 @@
 package com.fitness.activityService.dto;
 
+import com.fitness.activityService.models.ActivityType;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
 public class ActivityRequest {
+    private Long userId;
+    private ActivityType activityType;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+
 }
