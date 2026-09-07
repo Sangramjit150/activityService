79779802b8b415a350677fe924863bac683a667f
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-09-07 21:58:35 +0530

    This is the service logic for fetching userActivities

--- src/main/java/com/fitness/activityService/Service/ActivityService.java
+++ src/main/java/com/fitness/activityService/Service/ActivityService.java
@@ -7,6 +7,11 @@ import com.fitness.activityService.models.Activity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ActivityService {
@@ -18,7 +23,7 @@ public class ActivityService {
         activity.setDuration(activityRequest.getDuration());
         activity.setStartTime(activityRequest.getStartTime());
         activity.setType(activityRequest.getActivityType());
-
+        activity.setMetrics(activityRequest.getAdditionalMetrics());
 
         Activity savedActivity=activityRepository.save(activity);
 
@@ -31,6 +36,29 @@ public class ActivityService {
         activityResponse.setCreatedAt(savedActivity.getCreatedAt());
         activityResponse.setUpdatedAt(savedActivity.getUpdatedAt());
         activityResponse.setType(savedActivity.getType());
+        activityResponse.setAdditionalMetrics(savedActivity.getMetrics());
+        return activityResponse;
+    }
+
+
+    public List<ActivityResponse> getActivitiesByUserId(Long id) {
+        List<Activity>activities=activityRepository.findByUserid(id);
+
+        return activities.stream().map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    private ActivityResponse mapToResponse(Activity savedActivity) {
+        ActivityResponse activityResponse=new ActivityResponse();
+        activityResponse.setId(savedActivity.getId());
+        activityResponse.setUserid(savedActivity.getUserid());
+        activityResponse.setCaloriesBurned(savedActivity.getCaloriesBurned());
+        activityResponse.setDuration(savedActivity.getDuration());
+        activityResponse.setStartTime(savedActivity.getStartTime());
+        activityResponse.setCreatedAt(savedActivity.getCreatedAt());
+        activityResponse.setUpdatedAt(savedActivity.getUpdatedAt());
+        activityResponse.setType(savedActivity.getType());
+        activityResponse.setAdditionalMetrics(savedActivity.getMetrics());
         return activityResponse;
     }
 }
