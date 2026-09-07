a3dabee3a1ab4cfea07f99f0bfff707e43578d35
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-09-07 20:29:25 +0530

    This is the response dto

--- src/main/java/com/fitness/activityService/dto/ActivityResponse.java
+++ src/main/java/com/fitness/activityService/dto/ActivityResponse.java
@@ -1,4 +1,19 @@
 package com.fitness.activityService.dto;
 
+import com.fitness.activityService.models.ActivityType;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class ActivityResponse {
+    private Long id;
+    private Long userid;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
