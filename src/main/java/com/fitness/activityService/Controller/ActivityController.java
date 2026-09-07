443450d503a77ccdf1ced8b2d79db83bc126124f
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-09-07 21:57:49 +0530

    Built the controller for fetching the user activities

--- src/main/java/com/fitness/activityService/Controller/ActivityController.java
+++ src/main/java/com/fitness/activityService/Controller/ActivityController.java
@@ -6,10 +6,9 @@ import com.fitness.activityService.dto.ActivityResponse;
 import com.fitness.activityService.models.Activity;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/activities")
@@ -21,5 +20,10 @@ public class ActivityController {
     @PostMapping("/track")
     public ResponseEntity<ActivityResponse>trackActivity(@RequestBody ActivityRequest activityRequest){
         return ResponseEntity.ok(activityService.trackActivity(activityRequest));
+    }
+
+    @GetMapping("/getActivities/{id}")
+    public ResponseEntity<List<ActivityResponse>>getActivitiesByUserId(@PathVariable Long id){
+        return ResponseEntity.ok(activityService.getActivitiesByUserId(id));
     }
 }
