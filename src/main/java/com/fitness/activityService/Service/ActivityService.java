e4f1f04f769435cbce25666be7bb7a597ec56764
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-09-07 20:27:53 +0530

    This is the service where the business logic has been written

--- src/main/java/com/fitness/activityService/Service/ActivityService.java
+++ src/main/java/com/fitness/activityService/Service/ActivityService.java
@@ -1,4 +1,36 @@
 package com.fitness.activityService.Service;
 
+import com.fitness.activityService.Repository.ActivityRepository;
+import com.fitness.activityService.dto.ActivityRequest;
+import com.fitness.activityService.dto.ActivityResponse;
+import com.fitness.activityService.models.Activity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ActivityService {
+    private final ActivityRepository activityRepository;
+    public ActivityResponse trackActivity(ActivityRequest activityRequest) {
+        Activity activity=new Activity();
+        activity.setUserid(activityRequest.getUserId());
+        activity.setCaloriesBurned(activityRequest.getCaloriesBurned());
+        activity.setDuration(activityRequest.getDuration());
+        activity.setStartTime(activityRequest.getStartTime());
+        activity.setType(activityRequest.getActivityType());
+
+
+        Activity savedActivity=activityRepository.save(activity);
+
+        ActivityResponse activityResponse=new ActivityResponse();
+        activityResponse.setId(savedActivity.getId());
+        activityResponse.setUserid(savedActivity.getUserid());
+        activityResponse.setCaloriesBurned(savedActivity.getCaloriesBurned());
+        activityResponse.setDuration(savedActivity.getDuration());
+        activityResponse.setStartTime(savedActivity.getStartTime());
+        activityResponse.setCreatedAt(savedActivity.getCreatedAt());
+        activityResponse.setUpdatedAt(savedActivity.getUpdatedAt());
+        activityResponse.setType(savedActivity.getType());
+        return activityResponse;
+    }
 }
