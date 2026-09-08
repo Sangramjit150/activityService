c2d543fcf2e830f0f3c51d124c5af8892b81ba0f
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-09-08 20:30:47 +0530

    Validating the user through inter service communication

--- src/main/java/com/fitness/activityService/Service/UservalidationService.java
+++ src/main/java/com/fitness/activityService/Service/UservalidationService.java
@@ -1,4 +1,30 @@
 package com.fitness.activityService.Service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@RequiredArgsConstructor
 public class UservalidationService {
+    private final WebClient userServiceWebClient;
+    public boolean validateUser(Long userId){
+        try {
+            return userServiceWebClient.get()
+                    .uri("/api/users/{userId}/validate",userId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            if(e.getStatusCode()== HttpStatus.NOT_FOUND)
+            throw new RuntimeException("User Not found "+userId);
+            else if (e.getStatusCode()==HttpStatus.BAD_REQUEST) {
+                throw new RuntimeException("User doesnot exist :"+userId);
+            }
+        }
+    return false;
+    }
 }
